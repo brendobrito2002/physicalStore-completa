80515e90278e4cd5dcb783959fb63d241158336c
brendobrito2002 <brendoplayer@gmail.com>
2025-01-05 13:39:15 -0300

    configuracao basica das propriedades da store

--- src/stores/stores.entity.ts
+++ src/stores/stores.entity.ts
@@ -1,55 +1,73 @@
 import { Entity, Column, PrimaryGeneratedColumn } from "typeorm";
+import { IsString, IsBoolean, IsNumber, IsEmail } from "class-validator";
 
 @Entity()
 export class Store {
+    @IsString()
     @PrimaryGeneratedColumn()
     storeId: string;
 
+    @IsString()
     @Column()
     storeName: string;
 
+    @IsString()
     @Column()
     city: string;
 
+    @IsString()
     @Column()
-    district: string;
+    district: string; // bairro
 
+    @IsString()
     @Column()
     state: string;
 
+    @IsString()
     @Column()
     type: string; // PDV | Loja
 
+    @IsString()
     @Column()
     country: string;
 
+    @IsString()
     @Column()
     postalCode: string;
 
+    @IsString()
     @Column()
     teleponeNumber: string;
 
+    @IsEmail()
     @Column()
     emailAddress: string;
 
-    @Column()
+    @IsString()
+    @Column("decimal")
     latitude: string;
 
-    @Column()
+    @IsString()
+    @Column("decimal")
     longitude: string;
 
+    @IsString()
     @Column()
     address1: string;
 
-    @Column()
+    @IsString()
+    @Column({ nullable: true })
     address2: string;
 
-    @Column()
+    @IsString()
+    @Column({ nullable: true })
     address3: string;
 
-    @Column()
+    @IsBoolean()
+    @Column({ default: true })
     takeOutInStore: boolean; // sempre tem produto
 
-    @Column()
+    @IsNumber()
+    @Column("int")
     shippingTimeInDays: number; // considerar tempo de preparo
 }
