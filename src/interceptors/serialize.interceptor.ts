9f10308d2f7fa9849fe867153c48470d80a94ba2
brendobrito2002 <brendoplayer@gmail.com>
2025-01-06 11:54:46 -0300

    corrigido erro no update de loja, alocacao correta do uso dos dto, e codigo http de remove

--- src/interceptors/serialize.interceptor.ts
+++ src/interceptors/serialize.interceptor.ts
@@ -1,15 +1,24 @@
-import { UseInterceptors, NestInterceptor, ExecutionContext, CallHandler } from "@nestjs/common";
+import { NestInterceptor, ExecutionContext, CallHandler, UseInterceptors } from "@nestjs/common";
 import { Observable, map } from "rxjs";
-import { plainToClass } from "class-transformer";
-import { StoreResponseDto } from "src/stores/dtos/store-response.dto";
+import { plainToInstance } from "class-transformer";
+
+interface ClassContructor {
+    new(...args: any[]): {}
+}
+
+export function Serialize(dto: ClassContructor) {
+    return UseInterceptors(new SerializeInterceptor(dto));
+}
 
 export class SerializeInterceptor implements NestInterceptor {
+    constructor(private dto: any){}
+
     intercept(context: ExecutionContext, handler: CallHandler): Observable<any>{
         // Run something before a request is handled
         return handler.handle().pipe(
             map((data: any) => {
                 // Run something before the response is sent out
-                return plainToClass(StoreResponseDto, data, {
+                return plainToInstance(this.dto, data, {
                     excludeExtraneousValues: true,
                 });
             })
